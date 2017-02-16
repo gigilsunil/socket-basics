ca909886d161f32721d566bdca738c3f56d60fab
Gigil Mathew <gigil.sunil@gmail.com>
2017-02-16 13:20:51 +1000

    chat app

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,12 @@
 var socket =io();
 
-socket.on('connect',function()
+socket.on('connect',function()  //which is fired when we successfully connect to the server
 {
 	console.log('connected to socket.io server');
 });
+
+socket.on('message',function(message) //which will listen 
+{
+	console.log('New message');
+	console.log(message.text);
+})
