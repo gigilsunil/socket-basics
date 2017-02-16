405543c9845fcfa673e8cedb77e2f070d778d104
Gigil Mathew <gigil.sunil@gmail.com>
2017-02-16 14:12:38 +1000

    chat btwn two browsers

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,25 @@
-var socket =io();
+var socket = io();
 
-socket.on('connect',function()  //which is fired when we successfully connect to the server
-{
-	console.log('connected to socket.io server');
+socket.on('connect', function() //which is fired when we successfully connect to the server
+	{
+		console.log('connected to socket.io server');
+	});
+
+socket.on('message', function(message) //way for the frontend to listen to the customised event,'message'
+	{
+		console.log('New message');
+		console.log(message.text);
+	});
+
+//socket.emi('message',{text:'Hello'}) - we give this in the consol log of browser
+
+var $form = jQuery('#msgform');
+$form.on('submit', function(event) {
+	event.preventDefault();
+	socket.emit('message',
+	{
+		text : $form.find('input[name=message]').val()
+	});
+
+	$form.find('input[name=message]').val('');
 });
-
-socket.on('message',function(message) //which will listen 
-{
-	console.log('New message');
-	console.log(message.text);
-})
